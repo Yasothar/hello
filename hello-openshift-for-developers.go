a42322bc30c0166f434622d65c85419dacb36a50
Yasothar <ayasothar@gmail.com>
2023-07-13 17:10:46 +0530

    Update hello-openshift-for-developers.go
    test Commit

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello to Yasothar's world of OpenShift!!!"
+		response = "This is a test!!!"
 	}
 
 	fmt.Fprintln(w, response)
